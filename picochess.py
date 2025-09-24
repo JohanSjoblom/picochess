a785e86bb559c2b1fc61136a7c40f3f98f75dece
antonio <41113232+antmp27@users.noreply.github.com>
2025-09-24 18:18:25 +0200

    errores menores

--- picochess.py
+++ picochess.py
@@ -3280,8 +3280,8 @@ async def main() -> None:
                     (
                         self.login,
                         own_color,
-                        self.self.own_user,
-                        self.self.opp_user,
+                        self.own_user,
+                        self.opp_user,
                         self.game_time,
                         self.fischer_inc,
                     ) = read_online_user_info()
@@ -5065,3 +5065,4 @@ async def main() -> None:
 
 if __name__ == "__main__":
     asyncio.run(main())
+

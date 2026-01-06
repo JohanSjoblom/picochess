94a04725b22fb81f10eb955f89b134fa67b94fac
JohanSjoblom <messier109@gmail.com>
2026-01-06 19:34:34 +0200

    kiosk: detect Wayland and use minimal Chromium launch

--- kiosk.sh
+++ kiosk.sh
@@ -1,36 +1,46 @@
 #!/bin/bash
 
-xset s noblank
-xset s off
-xset -dpms
+is_wayland() {
+  [ "${XDG_SESSION_TYPE:-}" = "wayland" ] || [ -n "${WAYLAND_DISPLAY:-}" ]
+}
 
-# Uncomment this t rotate the DSI display to portrait
-# xrandr --output DSI-1 --rotate right
+if is_wayland; then
+  echo "kiosk.sh: Wayland session detected"
+else
+  echo "kiosk.sh: X11 session detected"
+  xset s noblank
+  xset s off
+  xset -dpms
 
-unclutter -idle 0.5 -root &
+  # Uncomment this to rotate the DSI display to portrait
+  # xrandr --output DSI-1 --rotate right
 
-if [ -d "/home/$USER/.config/chromium/Default" ] 
-then
-    sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/$USER/.config/chromium/'Local State'
-    sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/$USER/.config/chromium/Default/Preferences
+  unclutter -idle 0.5 -root &
 fi
 
-#  This will wait for PicoChess to start before launching the browser
+if [ -d "/home/$USER/.config/chromium/Default" ]; then
+  sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/$USER/.config/chromium/'Local State'
+  sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/$USER/.config/chromium/Default/Preferences
+fi
 
+# This will wait for PicoChess to start before launching the browser
 while true; do
   systemctl is-active --quiet picochess
   if [ $? -eq 0 ]; then
-    /usr/bin/chromium --enable-features=OverlayScrollbar --password-store=basic --display=:0 --noerrdialogs --disable-infobars --kiosk http://127.0.0.1 &
+    if is_wayland; then
+      /usr/bin/chromium --password-store=basic --kiosk http://127.0.0.1 &
+    else
+      /usr/bin/chromium --enable-features=OverlayScrollbar --password-store=basic --display=:0 --noerrdialogs --disable-infobars --kiosk http://127.0.0.1 &
+    fi
     exit 0
   else
     /bin/sleep 5
   fi
 done
 
-#  This setion will enable a refresh by chromium every 'sleep xx' seconds.
+# This section will enable a refresh by chromium every 'sleep xx' seconds.
 #
 #while true; do
-#   xdotool keydown ctrl+r; xdotool keyup ctrl+r;
-#   sleep 20
+#  xdotool keydown ctrl+r; xdotool keyup ctrl+r;
+#  sleep 20
 #done
-
